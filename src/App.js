a7ce3ad833d0dc35d507924040ca25b1654d26a1
Kevin Castro <kevincastro@C02FD2RV-0db637.local>
2023-02-09 09:12:29 -0700

    created the header section and included some components from zion. Ran the lint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Suspense } from 'react'
 import { Switch, Route, AuthRoute, NotFound } from '@fs/zion-router'
-import { LayoutBand, Tab, Tabs } from '@fs/zion-ui'
-import { useFeatureFlag } from '@fs/zion-flags'
 import ErrorBoundary from '@fs/zion-error-boundary'
 import HomePageSkeleton from './components/example/HomePageSkeleton'
 
@@ -10,31 +8,15 @@ import HomePageSkeleton from './components/example/HomePageSkeleton'
 
 const HomePage = React.lazy(() => import('./components/example/HomePage'))
 const UserInfoPage = React.lazy(() => import('./components/example/UserInfoPage'))
-const I18nPage = React.lazy(() => import('./components/example/I18nPage'))
-const FeatureFlagsPage = React.lazy(() => import('./components/example/FeatureFlagsPage'))
 
 function App() {
-  const frontier_craTemplate_flagTab = useFeatureFlag('frontier_craTemplate_flagTab')
-  const displayFlags = frontier_craTemplate_flagTab.treatment !== 'off' && !frontier_craTemplate_flagTab.isControl
   return (
     <ErrorBoundary>
       <Suspense fallback={<HomePageSkeleton />}>
-        <LayoutBand>
-          <Tabs>
-            <Tab title="Home" to="/" />
-            <Tab title="User Info" to="/user" />
-            <Tab title="I18n" to="/i18n" />
-            {displayFlags && <Tab title="flags" to="/flags" />}
-          </Tabs>
-        </LayoutBand>
-
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <AuthRoute path="/user" component={UserInfoPage} />
-          <Route path="/i18n" component={I18nPage} />
-          {displayFlags && <Route path="/flags" component={FeatureFlagsPage} />}
+          {/* <AuthRoute path="/user" component={UserInfoPage} /> */}
 
-          {/* blank page for performance testing purposes only */}
           <Route path="/blank" component={() => 'This page is intentionally left blank'} />
 
           <Route component={NotFound} />
--- src/components/example/HomePage.js
+++ src/components/example/HomePage.js
@@ -1,33 +1,13 @@
 import React, { Suspense } from 'react'
-import {
-  Body1,
-  colors,
-  DialogOverlay,
-  Grid,
-  FlowGrid,
-  Row,
-  HeaderBlock,
-  LayoutBand,
-  Separator,
-  Skeleton,
-  useContainerWidth,
-  useOverlay,
-} from '@fs/zion-ui'
+import { colors, HeaderBlock, LayoutBand, Separator, Skeleton, Tab, Tabs } from '@fs/zion-ui'
 
 import { Link } from '@fs/zion-router'
 import { css } from '@emotion/core'
 import zionDebug from '@fs/zion-debug'
 import { NoticeLoading } from '@fs/zion-icon'
-import ZionDesignCard from './ZionDesignCard'
-import LearnReactCard from './LearnReactCard'
-import PurposeStatementGenerator from './PurposeStatementGenerator'
-import WagonWheel from './WagonWheel'
-import ResponsiveDebug from './ResponsiveDebug'
-import RequireSignedInUser from './RequireSignedInUser'
 
-const debug = zionDebug('frontier:cra:example')
-const WagonWheelControl = React.lazy(() => import('./WagonWheelControl'))
-const ArtifactsViewer = React.lazy(() => import('./ArtifactsViewer'))
+import ProfileImg from './profile.jpeg'
+import BackgroundImg from './background.jpeg'
 
 const wagonButtonCss = css`
   cursor: pointer;
@@ -46,104 +26,54 @@ const wagonButtonCss = css`
     background-color: ${colors.gray02};
   }
 `
+
+const headerGridCss = css`
+  display: grid;
+  grid-template-rows: 210px 60px 90px;
+  grid-template-columns: 32px 150px auto 1fr;
+  column-gap: 12px;
+  margin-top: 32px;
+`
+
+const backgroundImgCss = css`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  grid-row: 1/3;
+  grid-column: 1/-1;
+  border-radius: 8px 8px 0 0;
+`
+
+const profileImgCss = css`
+  width: 100%;
+  border-radius: 50%;
+  grid-row: 2/4;
+  grid-column: 2/3;
+`
+
+const headerBlockCss = css`
+  grid-row: 3/4;
+  grid-column: 3/4;
+`
+
 const HomePage = () => {
-  // Initiate state variables and hooks
-  const atWidth = useContainerWidth()
-  const overlay = useOverlay({})
-  const [wheelColor, setWheelColor] = React.useState(colors.gray100)
-  const [wheelSpeed, setWheelSpeed] = React.useState('0s')
-
-  const handleWheelSpeedChange = React.useCallback(
-    (speed) => {
-      debug(`changing wheel animation speed: ${speed}`)
-      setWheelSpeed(speed)
-    },
-    [setWheelSpeed]
-  )
-
-  const handleWheelColorChange = React.useCallback(
-    (color) => {
-      debug(`changing wheel color: ${color}`)
-      setWheelColor(color)
-    },
-    [setWheelColor]
-  )
-
   return (
     <LayoutBand>
-      <Grid>
-        <HeaderBlock
-          alignY="middle"
-          columns={atWidth({ sm: 8 })}
-          size={atWidth({ default: 'md', lg: 'lg', xl: 'xl' })}
-          heading="This is the beginning of something amazing"
-          subHeading="Welcome to the start of your new Frontier application."
-        />
+      <header css={headerGridCss}>
+        <img src={BackgroundImg} alt="Laptop in desk" css={backgroundImgCss} />
+        <img src={ProfileImg} alt="Kevin Castro" css={profileImgCss} />
 
-        <button
-          columns={atWidth({ sm: 4 })}
-          aria-label="Configure wagon wheel"
-          css={wagonButtonCss}
-          type="button"
-          tabIndex={0}
-          onClick={overlay.handleClick}
-        >
-          <WagonWheel
-            alt="Wagon Wheel"
-            color={wheelColor}
-            animationDuration={wheelSpeed}
-            handleClick={overlay.handleClick}
-          />
-        </button>
-      </Grid>
-      <Separator />
-      <LayoutBand color={colors.gray02}>
-        <Separator size="xxs" />
-        <Body1>
-          To get started, take a look at the code in <code>src/App.js</code>. Ready to learn more? Check out the
-          resources below or visit the <Link to="https://beta.familysearch.org/frontier/docs">Frontier Docs</Link>.
-        </Body1>
+        <div css={headerBlockCss}>
+          <HeaderBlock size="md" heading="Kevin Castro" subHeading="Web Developer" />
+        </div>
+      </header>
+
+      <LayoutBand>
+        <Tabs>
+          <Tab title="Hobies" to="/hobies" />
+          <Tab title="Projects" to="/projects" />
+        </Tabs>
       </LayoutBand>
-      <Separator />
-      <FlowGrid gutters="md" columnCount={atWidth({ lg: 2 })}>
-        <ZionDesignCard />
-        <LearnReactCard />
-      </FlowGrid>
-      <Separator />
-      <PurposeStatementGenerator />
-      <Separator />
-      <React.Suspense fallback={<Skeleton.Image height={250} />}>
-        <RequireSignedInUser
-          Component={ArtifactsViewer}
-          fallback={
-            <LayoutBand color={colors.blue05}>
-              <Separator size="xxs" />
-              <Body1 centered>
-                We really want to show you some pictures of your ancestors but you must sign in first.
-              </Body1>
-            </LayoutBand>
-          }
-        />
-      </React.Suspense>
-      <Separator size="md" />
-      {/* Overlay */}
-      <DialogOverlay title="Wagon Wheel Controls" {...overlay}>
-        <Suspense
-          fallback={
-            <Row alignX="center">
-              <NoticeLoading size="md" />
-            </Row>
-          }
-        >
-          <WagonWheelControl
-            color={wheelColor}
-            animationDuration={wheelSpeed}
-            handleColorChange={handleWheelColorChange}
-            handleAnimationDurationChange={handleWheelSpeedChange}
-          />
-        </Suspense>
-      </DialogOverlay>
-      <ResponsiveDebug />
     </LayoutBand>
   )
 }
